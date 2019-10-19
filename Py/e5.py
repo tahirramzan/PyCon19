f8e61e686ece4de44e372a8b2dd811b3f5b1bf51
Tahir Ramzan <tahirramzan1@yahoo.com>
2019-10-19 13:34:02 +0500

    Update e5.py

--- Py/e5.py
+++ Py/e5.py
@@ -8,8 +8,6 @@ async def main():
     # with "main()".
     task = asyncio.create_task(nested())
 
-    # "task" can now be used to cancel "nested()", or
-    # can simply be awaited to wait until it is complete:
     print(await task)
 
 
